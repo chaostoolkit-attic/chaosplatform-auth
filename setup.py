8a82e0fc02e4ae3b8cc20eeb46b21a449040b2cf
Sylvain Hellegouarch <sh@defuze.org>
2018-12-20 18:33:37 +0100

    Fix trove
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -72,8 +72,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython'
-        'Topic :: Utilities',
     ],
     entry_points={
         'console_scripts': [
